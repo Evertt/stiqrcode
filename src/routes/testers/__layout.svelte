11f2b0fb451b0e9b851e5594abc573dbbe426deb
Evert van Brussel <ecvanbrussel@gmail.com>
2021-10-21 00:23:21 +0200

    Weird ass windicss bug

--- src/routes/testers/__layout.svelte
+++ src/routes/testers/__layout.svelte
@@ -26,7 +26,7 @@
 <div class="h-screen bg-light-300 w-screen top-0 left-0 fixed">
   <div class="bg-white rounded-md mx-auto max-w-md shadow-md m-4 p-4">
     {#if page !== 'login'}
-      <div class="m-2 text-center pb-2 hstack">
+      <div class="m-2 text-center pb-2 nav hstack">
         <a class:active={page === 'match'} sveltekit:prefetch href="/testers/match">Match</a>
         <a class:active={page === 'form'} sveltekit:prefetch href="/testers/form">Form</a>
       </div>
@@ -38,14 +38,13 @@
 </div>
 
 <style>
-  a {
-    border-bottom-width: 3px;
-    padding-bottom: 0.25rem;
-    width: 6rem;
+  .nav :global {
+    a {
+      @apply border-b-3 pb-1 w-24;
 
-    &.active {
-      --tw-border-opacity: 1;
-      border-color: rgba(30, 64, 175, var(--tw-border-opacity));
+      &.active {
+        @apply border-blue-800;
+      }
     }
   }
 
